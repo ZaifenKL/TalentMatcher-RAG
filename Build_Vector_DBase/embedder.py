a900d9be36f92c299235c34b1af212725366bd65
ZKL <zaifeng.king@gmail.com>
2026-06-26 10:19:20 -0700

    Tested embedder for user query with Huggingface transformer

--- Build_Vector_DBase/embedder.py
+++ Build_Vector_DBase/embedder.py
@@ -3,14 +3,13 @@ from sentence_transformers import SentenceTransformer
 ##------------------Constant Values---------------------------------------
 #Load Model
 #Load model a single time
-embedding_model = SentenceTransformer(r"C:\Users\User\.cache\huggingface\hub\models--sentence-transformers--paraphrase-multilingual-mpnet-base-v2")
-
-embedding_model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-mpnet-base-v2")##-------Define functions to optimize the pdf extraction data flow--------
+embedding_model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
+##-------Define functions to optimize the pdf extraction data flow--------
 ##------------------------------------------------------------------------
 def embed_text(text):
-    embedding = model.encode(text)
+    embedding = embedding_model.encode(text)
     return embedding.tolist()
 ##----DEBUG/TEST---------------------------------------------------------------
 text = "Hola podria buscar un ingeniero con 3 años de experiencia en Python?"
 embed_text = embed_text(text)
-print(f"This is my embedding : {embed_text}")
+#print(f"This is my embedding : {embed_text}")
