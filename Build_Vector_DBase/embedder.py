28e3f6b01e809df68b38b9ab90be0857c0bc6601
ZKL <zaifeng.king@gmail.com>
2026-06-25 21:11:27 -0700

    Tests on HuggingFace Tranformer for emb query

--- Build_Vector_DBase/embedder.py
+++ Build_Vector_DBase/embedder.py
@@ -1,14 +1,16 @@
-from openai import OpenAI
+from sentence_transformers import SentenceTransformer
 ##------------------------------------------------------------------------
 ##------------------Constant Values---------------------------------------
-client = OpenAI()
-#query_embedding es un vector the consulta
-##-------Define functions to optimize the pdf extraction data flow--------
+#Load Model
+#Load model a single time
+embedding_model = SentenceTransformer(r"C:\Users\User\.cache\huggingface\hub\models--sentence-transformers--paraphrase-multilingual-mpnet-base-v2")
+
+embedding_model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-mpnet-base-v2")##-------Define functions to optimize the pdf extraction data flow--------
 ##------------------------------------------------------------------------
 def embed_text(text):
-    #Converts user question/interaction into embedding
-        response = client.embeddings.create(
-            model="text-embedding-3-large",
-            input=text
-        )
-        return response.data[0].embedding
+    embedding = model.encode(text)
+    return embedding.tolist()
+##----DEBUG/TEST---------------------------------------------------------------
+text = "Hola podria buscar un ingeniero con 3 años de experiencia en Python?"
+embed_text = embed_text(text)
+print(f"This is my embedding : {embed_text}")
--- Build_Vector_DBase/vector_semantic_search.py
+++ Build_Vector_DBase/vector_semantic_search.py
@@ -6,7 +6,6 @@ k_chunks = 5
 ##-------Define functions to optimize the pdf extraction data flow--------
 ##------------------------------------------------------------------------
 
-
 def search_similar_chunks(collection, query_embedding,k_chunks):
     #This is the semantic search function based on vectors, it searches for the most similar chunks using coseno
     #returns a dictionary
